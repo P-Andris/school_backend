d656b2be04b2851fbca333de93646ee1c97a9a08
P-Andris <pillerandras0714@gmail.com>
2022-09-20 08:10:19 +0200

    Adatbázis létrehozás

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            // Auto-increment, Primary key, Big integer
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamps();
+        });
+
+        // Rekordok ide kerülnek:
+        User::create(['name' => 'Klári', 'email' => 'diak1@gmail.com']);
+        User::create(['name' => 'Pisti', 'email' => 'diak2@gmail.com']);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
